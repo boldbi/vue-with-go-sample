f1682ccf25912547a8fd8440294a7fce28194b58
dharshinivivek <dharshini.sf4021@gmail.com>
2023-07-10 09:18:41 +0530

    Added to code get the values from embedconfig.json

---
+++ Go/Client/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app" ref="app">
+    <div id="dashboard" ref="dashboard">
+      <b-modal v-model="showErrorModal" title="Error" centered>
+        <p class="error-message">{{ errorMessage }}</p>
+      </b-modal>
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery';
+import { BoldBI } from '@boldbi/boldbi-embedded-sdk';
+import axios from 'axios';
+
+window.jQuery = $;
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      errorMessage: '',
+    };
+  },
+  async mounted() {
+    var scripts = [
+      'https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js',
+    ];
+    scripts.forEach((script) => {
+      let tag = document.createElement('script');
+      tag.setAttribute('src', script);
+      tag.setAttribute('type', 'text/javascript');
+      tag.setAttribute('defer', 'defer');
+      tag.async = true;
+      document.head.appendChild(tag);
+    });
+
+    let authorizationUrl = 'http://localhost:8086/getDetails';
+
+    try {
+      const response = await axios.get('http://localhost:8086/parseJson');
+
+      console.log(response.data);
+      if(response.data=="")
+      {
+       this.errorMessage = 'Error: Failed to load embedConfig.json';
+       this.showErrorModal = true;
+      }
+      else{
+        createBoldBIDashboard(response.data);
+      }      
+    } catch (error) {
+      console.log(error);
+      this.errorMessage = 'Error: Failed to load embedConfig.json';
+      this.showErrorModal = true;
+    }
+
+    function createBoldBIDashboard(data) {
+      let dashboard = BoldBI.create({
+        serverUrl: data.ServerUrl + '/' + data.SiteIdentifier,
+        dashboardId: data.DashboardId,
+        embedContainerId: 'dashboard',
+        embedType: BoldBI.EmbedType.Component,
+        environment: data.Environment,
+        width: '100%',
+        height: window.innerHeight + 'px',
+        expirationTime: 100000,
+        authorizationServer: {
+          url: authorizationUrl,
+        },
+      });
+      dashboard.loadDashboard();
+      console.log(dashboard);
+    }
+  },
+};
+</script>
+
+<style>
+.error-message {
+  color: red;
+  text-align: center;
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+</style>
--- Go/main.go
+++ Go/main.go
@@ -8,28 +8,126 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	//"runtime/debug"
 	"strconv"
 	"strings"
 	"time"
 )
 
 //Set EmbedSecret key from Bold BI Server. Please refer this link(https://help.syncfusion.com/bold-bi/on-premise/site-settings/embed-settings)
-var embedSecret = ""
+var embedSecret = "iUgYqIhFooyeI5HPRL9siUclfIAZy4eT"
 
 //Enter your BoldBI Server credentials
-var userMail = ""
+var userMail = "dharshini.v@syncfusion.com"
 
 func main() {
+	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/parseJson", parseJSONHandler)
 	http.HandleFunc("/getDetails", getEmbedDetails)
-	log.Fatal(http.ListenAndServe(":8086", nil))
 
+	go func() {
+		if err := http.ListenAndServe(":8086", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	log.Println("Go server is running on port 8086")
+
+	// Keep the main goroutine alive
+	select {}
 }
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("App is running!"))
+}
+
+// func parseJSONHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Context-Type", "application/json")
+// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+// 	w.Header().Set("Access-Control-Allow-Methods", "POST")
+// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		log.Println("Error reading request body:", err)
+// 		http.Error(w, "Error reading request body", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	var jsonData interface{}
+// 	err = json.Unmarshal(body, &jsonData)
+// 	if err != nil {
+// 		log.Println("Error parsing JSON:", err)
+// 		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	// Process the received JSON data as needed
+// 	// ...
+
+// 	response := map[string]interface{}{
+// 		"status":  "success",
+// 		"message": "JSON data received and processed successfully",
+// 	}
+
+// 	jsonResponse, err := json.Marshal(response)
+// 	if err != nil {
+// 		log.Println("Error creating JSON response:", err)
+// 		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Write(jsonResponse)
+// }
+func parseJSONHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// Read the embedConfig.json file
+	filePath := "embedConfig.json"
+	fileData, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Println("embedConfig file is not found")
+		//http.Error(w, "Error reading", http.StatusInternalServerError)
+		return
+	}
+
+	var jsonData interface{}
+	err = json.Unmarshal(fileData, &jsonData)
+	if err != nil {
+		log.Println("Error parsing JSON:", err)
+		http.Error(w, "Error parsing JSON", http.StatusInternalServerError)
+		return
+	}
+
+	if jsonData == nil {
+		log.Println("Received JSON data is nil")
+		http.Error(w, "Received JSON data is nil", http.StatusBadRequest)
+		return
+	}
+
+	// Process the received JSON data as needed
+	// ...
+
+	response := jsonData.(map[string]interface{})
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		log.Println("Error creating JSON response:", err)
+		http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonResponse)
+}
 func getEmbedDetails(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatalln(err)
@@ -37,6 +135,8 @@ func getEmbedDetails(w http.ResponseWriter, r *http.Request) {
 	if len(body) > 0 {
 		if queryString, err := unmarshal(string(body)); err != nil {
 			log.Println("error converting", err)
+			http.Error(w, "Error converting JSON", http.StatusBadRequest)
+			return
 		} else {
 			serverAPIUrl := queryString.(map[string]interface{})["dashboardServerApiUrl"].(string)
 			embedQueryString := queryString.(map[string]interface{})["embedQuerString"].(string)
@@ -50,11 +150,15 @@ func getEmbedDetails(w http.ResponseWriter, r *http.Request) {
 			result, err := http.Get(query)
 			if err != nil {
 				log.Println(err)
+				http.Error(w, "Error calling API", http.StatusInternalServerError)
+				return
 			}
 			log.Println(result)
 			response, err := ioutil.ReadAll(result.Body)
 			if err != nil {
 				log.Fatalln(err)
+				http.Error(w, "Error reading response", http.StatusInternalServerError)
+				return
 			}
 			w.Write(response)
 		}
